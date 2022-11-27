5446d6f228b650e8c51cd744985377d0d980642d
Yahaya Bathily <developpeur.yb@gmail.com>
2022-11-27 19:41:17 +0100

    add vendor to base structure

---
+++ bin/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6326bbf73b7f70fc661b0633a14c387c62c021b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6326bbf73b7f70fc661b0633a14c387c62c021b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
